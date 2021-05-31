7cfe7f0f93bfd5ea270169e084447061fa261529
Jan Böker <naj_b@gmx.de>
2021-05-31 21:31:43 +0200

    first guess solving the challange

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleInputFistName(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleInputLastName(event) {
+    setLastName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {firstName} {lastName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={handleInputFistName} name="fName" placeholder="First Name" />
+        <input onChange={handleInputLastName} name="lName" placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
